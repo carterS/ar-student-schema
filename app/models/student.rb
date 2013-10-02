a9b95ca8efc47935d61eadd923e08b501befb2da
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-10-02 15:40:39 -0700

    migrated db

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,22 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  
+  def initialize(attributes = {})
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @gender = attributes[:gender]
+    @birthday = attributes[:birthday]
+    @email = attributes[:email]
+    @phone = attributes[:phone]
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
+
+  def age
+    DateTime.now - DateTime.parse(@birthday)
+  end
 end
+
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -5,5 +5,15 @@ require_relative '../config'
 class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |field|
+      field.string :first_name
+      field.string :last_name
+      field.string :gender
+      field.date :birthday
+      field.string :email
+      field.string :phone
+
+      field.timestamps
+    end
   end
 end
